ec86554f93f5b9497d20991d5c0fdedb9742d38f
jhon11390 <meljhon2@gmail.com>
2020-05-19 11:35:10 -0500

    Crea formulario new

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,25 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
   end
 
   def new
+    @product = Product.new
+    @categories = Category.all
   end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to :products
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def product_params
+      params.require(:product).permit(:name, :price)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "/products", to: 'products#index'
   get "/products/new", to: 'products#new'
+  post "/products" , to: "products#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
